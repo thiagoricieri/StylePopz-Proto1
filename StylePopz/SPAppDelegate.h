20b636f1d91c1d4ce1b213beb2e2aab85e062ea8
Thiago Ricieri <thiago.ricieri@gmail.com>
2013-07-02 11:02:42 -0300

    Initial Commit

---
+++ StylePopz/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  StylePopz
+//
+//  Created by Thiago Ricieri on 02/07/13.
+//  Copyright (c) 2013 Thiago Ricieri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StylePopz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StylePopz
+//
+//  Created by Thiago Ricieri on 02/07/13.
+//  Copyright (c) 2013 Thiago Ricieri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
